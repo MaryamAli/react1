3e6ec1171e2274f1c994344bfa28f9e19a572102
Maryam Ali <mali62541@gmail.com>
2016-05-14 00:05:56 -0400

    single view still working

---
+++ js/parseData.js
@@ -0,0 +1,9 @@
+const APP_ID = '88arqa0AV2h7KjKrheweb8wosV4JBFgzetjgcCLg';
+const API_KEY = '1ZSbvmwoUxkUIE64EkWimNDp3SDW9hJDG0tvoW4n';
+const APP_URL = 'https://api.parse.com/1/classes/MyPeeps';
+
+export {
+  APP_URL,
+  API_KEY,
+  APP_ID
+};
